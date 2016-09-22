e79d1abd5cb6e835f29182493d071cef58310a09
Animesh Baranawal <animeshbaranawal@gmail.com>
2016-09-22 23:04:45 +0530

    Pictures generated for both merges

--- script.sh
+++ script.sh
@@ -1,12 +1,9 @@
 make clean
 make
 
-./trace ./data/scene1.scd out1.png
-./trace ./data/scene2.scd out2.png
-./trace ./data/scene4.scd out4.png
-./trace ./data/scene5.scd out5.png
-
-./trace ./data/scene1c.scd out1c.png
-./trace ./data/scene2c.scd out2c.png
-./trace ./data/scene4c.scd out4c.png
 ./trace ./data/scene5c.scd out5c.png
+./trace ./data/scene3c.scd out3c.png
+
+cd ./data
+../trace teapot.scd ../teapot.png
+../trace dunkit.scd ../dunkit.png
